1c28cc87341310c5c4c6f2d6b3da7e8329aba6da
admin <admin@YD05-09>
2024-04-22 14:13:30 +0900

    vo 수정

--- musinsam/src/main/java/com/yedam/vo/ClothesVO.java
+++ musinsam/src/main/java/com/yedam/vo/ClothesVO.java
@@ -12,5 +12,5 @@ public class ClothesVO {
 	private int discountRate;
 	private String csize;
 	private int cnt;
-	
+	private String img;
 }
--- musinsam/src/main/java/com/yedam/vo/StockVO.java
+++ musinsam/src/main/java/com/yedam/vo/StockVO.java
@@ -6,5 +6,6 @@ import lombok.Data;
 public class StockVO {
 	private int clothNo; //옷번호
 	private int quantity; //옷 재고 양
+	private String csize; //옷사이즈
 
 }
