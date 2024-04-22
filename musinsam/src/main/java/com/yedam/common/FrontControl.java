7e09ea6ead1b604a7668f9d7dceeb0c5a2820f7c
admin <sdhan1360@gmail.com>
2024-04-22 15:49:13 +0900

    ..

---
+++ musinsam/src/main/java/com/yedam/common/FrontControl.java
@@ -0,0 +1,40 @@
+package com.yedam.common;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yedam.control.StartControl;
+
+import com.yedam.common.Control;
+
+public class FrontControl extends HttpServlet {
+	Map<String, Control> map;
+	
+	public FrontControl() {
+		map = new HashMap<>();
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		map.put("/main.do", new StartControl());
+	}
+	
+	@Override
+	protected void service(HttpServletRequest reqeust, HttpServletResponse response) throws ServletException, IOException {
+		reqeust.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		String uri = reqeust.getRequestURI();
+		String context = reqeust.getContextPath();
+		String path = uri.substring(context.length());
+		
+		Control control = map.get(path);
+		control.execute(reqeust, response);
+	}
+}
