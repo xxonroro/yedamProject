19e2499e1e9e514bb5d0d03be52c3b5c5a22c306
Ejfq <161672957+Ejfq@users.noreply.github.com>
2024-04-24 09:48:18 +0900

    Delete musinsam directory

--- musinsam/src/main/java/com/yedam/board/todayCoodiMapper.java
+++
@@ -1,9 +0,0 @@
-package com.yedam.board;
-
-import java.util.List;
-
-public interface todayCoodiMapper {
-	public List<ClothesVO> randomTop();
-	public List<ClothesVO> randomBottom();
-	public List<ClothesVO> randomShoes();
-}
