b375af31d26e3a7f384f06f8dcc388fd891d44e4
Ejfq <161672957+Ejfq@users.noreply.github.com>
2024-04-24 09:39:15 +0900

    Delete musinsam directory

--- musinsam/src/main/java/com/yedam/product/ProductMapper.java
+++
@@ -1,6 +0,0 @@
-package com.yedam.product;
-
-public interface ProductMapper {
-	public ClothesVO getCloth (int cno); //옷 단건 조회
-	
-}
--- musinsam/src/main/java/com/yedam/product/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.yedam.product;
-
-import java.util.List;
-
-public interface ProductService {
-
-	ClothesVO getProduct(int cno);
-
-}
