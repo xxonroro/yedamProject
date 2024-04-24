6a955763ac8629887c1387696aff060e2a98ebb2
xxonroro <sdhan1360@gmail.com>
2024-04-24 17:48:43 +0900

    ..

--- musinsam/src/main/java/com/yedam/common/FrontControl.java
+++ musinsam/src/main/java/com/yedam/common/FrontControl.java
@@ -31,13 +31,23 @@ public class FrontControl extends HttpServlet {
 
 	@Override
 	public void init() throws ServletException {
-		// 메인 페이지 (이거에요 지우지마세요. start 뭐시기 그거 지워주세요
+		// 메인 페이지
 		map.put("/main.do", new MainControl());
 
 
+		// 동한 (start)
 		// 회원 가입
 		map.put("/memberRegisterForm.do", new MemberRegisterForm());
 		map.put("/memberRegistration.do", new MemberRegistration());
+		
+		
+		
+		
+		
+		// 동한 (end)
+		
+		
+		
 		
 		map.put("/cart.do",	new Cart());
 		map.put("/cartList.do", new CartList());
--- musinsam/src/main/webapp/WEB-INF/layouts/mainPage/mainTemplate.jsp
+++ musinsam/src/main/webapp/WEB-INF/layouts/mainPage/mainTemplate.jsp
@@ -19,6 +19,8 @@
 	href="vendors/owl-carousel/owl.theme.default.min.css">
 <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
 
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
